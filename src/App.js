2908bc371009537b398f990fd691cfd4ff153d3f
jeons <etoile929@naver.com>
2024-07-06 22:17:25 +0900

    채팅앱 프론트엔드 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import socket from "./server";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
 
 function App() {
     const [user, setUser] = useState(null);
+    const [message, setMessage] = useState('');
+    const [messageList, setMessageList] = useState([]);
+
+    console.log("message List", messageList);
       useEffect(() => {
+          socket.on('message',(message) => {
+              setMessageList((prevState) => prevState.concat(message));
+          });
           askUserName();
       },[]);
   const askUserName = () => {
@@ -18,9 +27,23 @@ function App() {
       });
   }
 
+  const sendMessage = (event) => {
+      event.preventDefault()
+      socket.emit("sendMessage", message, (res) => {
+        console.log("sendMessage res",res);
+      }); 
+  };
+
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+      <MessageContainer messageList={messageList} user={user}/>
+        <InputField 
+            message={message} 
+            setMessage={setMessage} 
+            sendMessage={sendMessage}
+        />
+      </div>
     </div>
   );
 }
